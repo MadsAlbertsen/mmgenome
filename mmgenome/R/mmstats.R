6fd4a86b09a5d100ecc619ddcdc5d1f143070774
Rasmus Kirkegaard <rasmus.kirkegaard@hotmail.com>
2016-11-24 12:54:39 +0100

    Update mmstats.R
    Included an option to provide the original dataframe along with the bin to show the fraction of reads the bin accounts for in each of the coverage files instead of just the coverage. Makes it easier to interpret in which sample the bin is enriched.

--- mmgenome/R/mmstats.R
+++ mmgenome/R/mmstats.R
@@ -4,8 +4,9 @@
 #'
 #' @usage mmstats(data, ncov)
 #'
-#' @param data The dataframe containing all data.
+#' @param data The dataframe containing the bin data (e.g. dA).
 #' @param ncov The number of coverage datasets (default = 2).
+#' @param data_compare The dataframe containing the all data (e.g. d).
 #' 
 #' @return An overview of key statistics of the scaffolds.
 #' 
@@ -20,7 +21,7 @@
 #' mmstats(data = d, ncov = 4)
 #' }
 
-mmstats <- function(data, ncov=2) {
+mmstats <- function(data, ncov=2,data_compare=NULL) {
   options(scipen=8)
   data$scaffolds$length <- as.numeric(data$scaffolds$length)
   coverage <- list()
@@ -46,6 +47,21 @@ mmstats <- function(data, ncov=2) {
       break
     }
   }
+  # If there is  a data object to compare it to:
+  if (!is.null(data_compare)) {
+    data_compare$scaffolds$length <- as.numeric(data_compare$scaffolds$length)
+    coverage_compare <- list()
+    for (i in 1:ncov){
+      coverage_compare[i] <- round(sum((data_compare$scaffolds[,i+3]*data_compare$scaffolds$length))/sum(data_compare$scaffolds$length),2)
+      names(coverage_compare)[i] <- paste("Coverage",names(data$scaffolds)[i+3])
+    }
+    coverage_compare<-t(data.frame(coverage_compare))
+    Length.total_compare <- sum(data_compare$scaffolds$length)
+    fractions<-round(Length.total/Length.total_compare*(coverage/coverage_compare)*100,2)
+    row.names(fractions)<-gsub(pattern = "Coverage",replacement = "Fraction(%)",x = row.names(fractions))
+    coverage<-fractions
+  }
+  
   
   if (length(data) == 2){
     out<-rbind(n.scaffolds, GC.mean, N50, Length.total, Length.max, Length.mean, coverage, Ess.total, Ess.unique)
